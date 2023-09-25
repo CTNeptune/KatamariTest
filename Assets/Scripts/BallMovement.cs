043342701326469be3fd77349d3e611ebdf17fa2
CTNeptune <tombocat9@gmail.com>
2023-09-25 13:52:11 -0400

    Setup Ball Movement Relative To Camera

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -7,10 +7,18 @@ using Cinemachine;
 
 public class BallMovement : MonoBehaviour
 {
-    public CinemachineFreeLook _FreeLookCamera;
+    public Camera _Camera;
+    public CinemachineFreeLook _FreeLookComponent;
+    public Rigidbody _BallRigidbody;
+    public float _BallSpeed;
 
     private KatamariInput mInput;
+    private Vector2 mMoveInput;
 
+    /// <summary>
+    /// In start we create a new instance of KatamariInput, a class generated with Unity's new Input System.
+    /// Subscribe our OnMoveBall and OnMoveCamera methods to the MoveBall and MoveCamera events raised from KatamariInput, then enable the input.
+    /// </summary>
     private void Start()
     {
         mInput = new KatamariInput();
@@ -20,16 +28,56 @@ public class BallMovement : MonoBehaviour
         mInput.Enable();
     }
 
-    private void OnMoveBall(InputAction.CallbackContext obj)
+    /// <summary>
+    /// Updates the movement input vector. Subscribed to the mInput.Ball.MoveBall.Performed event.
+    /// </summary>
+    /// <param name="inCallback"></param>
+    private void OnMoveBall(InputAction.CallbackContext inCallback)
     {
-        throw new NotImplementedException();
+        mMoveInput = inCallback.ReadValue<Vector2>();
     }
 
-    private void OnMoveCamera(InputAction.CallbackContext obj)
+    /// <summary>
+    /// Continually add force to the sphere based on mMoveInput
+    /// </summary>
+    private void Update()
     {
-        Vector2 inputDelta = obj.ReadValue<Vector2>();
+        if (mMoveInput == Vector2.zero)
+            return;
 
-        _FreeLookCamera.m_XAxis.Value += inputDelta.x;
-        _FreeLookCamera.m_YAxis.Value += inputDelta.y;
+        Vector3 moveDirection = CalculateRelativeMoveDirection(mMoveInput);
+        Vector3 finalMove = moveDirection * _BallSpeed * mMoveInput.magnitude;
+
+        _BallRigidbody.AddForce(finalMove, ForceMode.Force);
+    }
+
+    /// <summary>
+    /// Calculates how the ball should be moved relative to the camera's direction
+    /// </summary>
+    /// <param name="inputDirection"></param>
+    /// <returns></returns>
+    private Vector3 CalculateRelativeMoveDirection(Vector2 inputDirection)
+    {
+        Vector3 cameraForward = _Camera.transform.forward;
+        Vector3 cameraRight = _Camera.transform.right;
+
+        Vector3 moveDirection = (cameraForward * inputDirection.y) + (cameraRight * inputDirection.x);
+        moveDirection.y = 0f;
+
+        moveDirection.Normalize();
+
+        return moveDirection;
+    }
+
+    /// <summary>
+    /// Moves the camera. Subscribed to the mInput.Ball.MoveCamera.performed event.
+    /// </summary>
+    /// <param name="inCallback"></param>
+    private void OnMoveCamera(InputAction.CallbackContext inCallback)
+    {
+        Vector2 inputDelta = inCallback.ReadValue<Vector2>();
+
+        _FreeLookComponent.m_XAxis.Value += inputDelta.x;
+        _FreeLookComponent.m_YAxis.Value += inputDelta.y;
     }
 }
